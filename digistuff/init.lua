c0dc30ddbc3ae6ebe3ee66614951a8368b3c7533
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2019-12-02 17:11:10 +0000

    add digistuff

---
+++ digistuff/init.lua
@@ -0,0 +1,27 @@
+digistuff = {}
+
+local components = {
+	"internal",
+	"conductors",
+	"touchscreen",
+	"light",
+	"noteblock",
+	"camera",
+	"switches",
+	"panel",
+	"piezo",
+	"detector",
+	"piston",
+	"timer",
+}
+for _,name in ipairs(components) do
+	dofile(string.format("%s%s%s.lua",minetest.get_modpath(minetest.get_current_modname()),DIR_DELIM,name))
+end
+
+local http = minetest.request_http_api()
+if not http then
+	minetest.log("error","digistuff is not allowed to use the HTTP API - digilines NIC will not be available!")
+	minetest.log("error","If this functionality is desired, please add digistuff to your secure.http_mods setting")
+else
+	loadfile(string.format("%s%s%s.lua",minetest.get_modpath(minetest.get_current_modname()),DIR_DELIM,"nic"))(http)
+end
