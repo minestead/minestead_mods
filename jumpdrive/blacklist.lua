5dc5e3a130fe4f6f50cc86554cb1731c01265c6e
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2019-12-18 10:11:06 +0000

    jumpdrive blacklist: tubelib forceload; advtrains

--- jumpdrive/blacklist.lua
+++ jumpdrive/blacklist.lua
@@ -5,6 +5,8 @@ end
 
 if minetest.get_modpath("advtrains") then
 	table.insert(jumpdrive.blacklist, "advtrains:dtrack_atc_st")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_bumper_st")
+
 	table.insert(jumpdrive.blacklist, "advtrains:dtrack_cr")
 	table.insert(jumpdrive.blacklist, "advtrains:dtrack_cr_30")
 	table.insert(jumpdrive.blacklist, "advtrains:dtrack_cr_45")
@@ -16,8 +18,31 @@ if minetest.get_modpath("advtrains") then
 	table.insert(jumpdrive.blacklist, "advtrains:dtrack_st_60")
 	table.insert(jumpdrive.blacklist, "advtrains:dtrack_vst1")
 	table.insert(jumpdrive.blacklist, "advtrains:dtrack_vst2")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swlcr")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swlcr_30")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swlcr_60")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swrcr")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swrcr_30")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swrcr_60")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swlst")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swlst_30")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swlst_60")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swrst")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swrst_30")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_swrst_60")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_vst1")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_vst2")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_vst31")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_vst32")
+	table.insert(jumpdrive.blacklist, "advtrains:dtrack_vst33")
+	table.insert(jumpdrive.blacklist, "advtrains:platform_high_sandstonebrick")
+	table.insert(jumpdrive.blacklist, "advtrains:platform_low_sandstonebrick")
 	table.insert(jumpdrive.blacklist, "advtrains:platform_high_stonebrick")
 	table.insert(jumpdrive.blacklist, "advtrains:platform_low_stonebrick")
+	table.insert(jumpdrive.blacklist, "advtrains:signal_on")
+	table.insert(jumpdrive.blacklist, "advtrains:signal_off")
+	table.insert(jumpdrive.blacklist, "advtrains_interlocking:tcb_node")
+	table.insert(jumpdrive.blacklist, "advtrains_line_automation:dtrack_stop_st")
 end
 
 if minetest.get_modpath("moremesecons_wireless") then
@@ -31,5 +56,7 @@ end
 
 if minetest.get_modpath("tubelib_addons3") then
 	table.insert(jumpdrive.blacklist, "tubelib_addons3:teleporter")
+	table.insert(jumpdrive.blacklist, "tubelib:forceload")
 end
+
 -- TODO bedrock, advtrains tracks
