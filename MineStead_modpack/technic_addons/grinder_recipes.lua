f2de2eb164cb90b78f919992cc39cd5a472e0677
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2020-01-01 19:37:59 +0000

    add grinder recipes for caverealms

--- MineStead_modpack/technic_addons/grinder_recipes.lua
+++ MineStead_modpack/technic_addons/grinder_recipes.lua
@@ -7,6 +7,13 @@ local recipes = {
         {"default:silver_sand",             "default:clay"},
 }
 
+if minetest.get_modpath("caverealms") then
+    table.insert(recipes, {"caverealms:salt_crystal", "farming:salt 4"})
+    table.insert(recipes, {"caverealms:stone_with_salt", "farming:salt 2"})
+    table.insert(recipes, {"caverealms:salt_gem", "farming:salt 1"})
+    table.insert(recipes, {"caverealms:glow_mese", "default:mese_crystal_fragment 5"})
+end
+
 for _, data in pairs(recipes) do
         technic.register_grinder_recipe({input = {data[1]}, output = data[2]})
 end
