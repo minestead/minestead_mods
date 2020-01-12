89a0ad7374c0814135e3f357cd035c56a975b935
techniX <sergei.mozhaisky@gmail.com>
2020-01-12 12:30:34 +0200

    update fnuclear

---
+++ MineStead_modpack/fnuclear/README.txt
@@ -0,0 +1,12 @@
+Minetest uranium tools Mod by Irremann
+
+Add uranium tools: axe, crowbar, pickaxe, shovel, sword, hammer, sickle. Add uranium lamp and barrel for toxic waste.
+They all created from depleted uranium 0%. All these tools glow in the dark (required wielded_light mod).
+The sickle works like a scythe from the farming mod. The hammer smashes any cobblestone and any stone into gravel. And gravel and compressed gravel into sand.
+Also added several recipes for disassembling diamond and mese tools using a hammer.
+
+
+LICENSE: WTFPL
+
+the ideas of tools from the Uranium Mod by Author: LandMine
+barrel model from Wine mod by TenPlus1
--- MineStead_modpack/fnuclear/init.lua
+++ MineStead_modpack/fnuclear/init.lua
@@ -23,7 +23,7 @@ minetest.register_tool("fnuclear:uranium_pick", {
 		full_punch_interval = 0.9,
 		max_drop_level=3,
 		groupcaps={
-			cracky = {times={[1]=2.0, [2]=1.0, [3]=0.50}, uses=30, maxlevel=3},
+			cracky = {times={[1]=2.0, [2]=1.0, [3]=0.50}, uses=100, maxlevel=3},
 		},
 		damage_groups = {fleshy=5},
 	},
@@ -48,7 +48,7 @@ minetest.register_tool("fnuclear:uranium_axe", {
 		full_punch_interval = 0.9,
 		max_drop_level=1,
 		groupcaps={
-			choppy={times={[1]=2.10, [2]=0.90, [3]=0.50}, uses=30, maxlevel=3},
+			choppy={times={[1]=2.10, [2]=0.90, [3]=0.50}, uses=100, maxlevel=3},
 		},
 		damage_groups = {fleshy=7},
 	},
@@ -73,7 +73,7 @@ minetest.register_tool("fnuclear:uranium_shovel", {
 		full_punch_interval = 1.0,
 		max_drop_level=1,
 		groupcaps={
-			crumbly = {times={[1]=1.10, [2]=0.50, [3]=0.30}, uses=30, maxlevel=3},
+			crumbly = {times={[1]=1.10, [2]=0.50, [3]=0.30}, uses=100, maxlevel=3},
 		},
 		damage_groups = {fleshy=4},
 	},
@@ -98,7 +98,7 @@ minetest.register_tool("fnuclear:uranium_sword", {
 		full_punch_interval = 0.7,
 		max_drop_level=1,
 		groupcaps={
-			snappy={times={[1]=1.90, [2]=0.90, [3]=0.30}, uses=40, maxlevel=3},
+			snappy={times={[1]=1.90, [2]=0.90, [3]=0.30}, uses=100, maxlevel=3},
 		},
 		damage_groups = {fleshy=8},
 	},
@@ -123,7 +123,7 @@ minetest.register_tool("fnuclear:uranium_crowbar", {
 		full_punch_interval = 0.9,
 		max_drop_level=1,
 		groupcaps={
-			choppy={times={[1]=2.10, [2]=0.90, [3]=0.50}, uses=50, maxlevel=3},
+			choppy={times={[1]=2.10, [2]=0.90, [3]=0.50}, uses=100, maxlevel=3},
 		},
 		damage_groups = {fleshy=10},
 	},
@@ -192,4 +192,241 @@ minetest.register_craft({
 		{"technic:lead_ingot", "", "technic:lead_ingot"},
 		{"technic:lead_ingot", "technic:lead_ingot", "technic:lead_ingot"},
 	},
+})
+
+minetest.register_tool("fnuclear:uranium_sickle", {
+	description = "Uranium Sickle",
+	inventory_image = "uranium_sickle.png",
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level=2,
+		groupcaps={
+			fleshy = {times = {[2] = 0.65, [3] = 0.25}, uses = 500, maxlevel = 2},
+			snappy = {times = {[2] = 0.70, [3] = 0.25}, uses = 500, maxlevel = 2},
+		},
+		damage_groups = {fleshy=8},
+	},
+	paramtype = "light",
+	light_source = 12,
+	sound = {breaks = "default_tool_breaks"},
+	on_place = function(itemstack, placer, pointed_thing)
+
+		if pointed_thing.type ~= "node" then
+			return
+		end
+
+		local pos = pointed_thing.under
+		local name = placer:get_player_name()
+
+		if minetest.is_protected(pos, name) then
+			return
+		end
+
+		local node = minetest.get_node_or_nil(pos)
+
+		if not node then
+			return
+		end
+
+		local def = minetest.registered_nodes[node.name]
+
+		if not def then
+			return
+		end
+
+		if not def.drop then
+			return
+		end
+
+		if not def.groups
+		or not def.groups.plant then
+			return
+		end
+
+		local drops = minetest.get_node_drops(node.name, "")
+
+		if not drops
+		or #drops == 0
+		or (#drops == 1 and drops[1] == "") then
+			return
+		end
+
+		-- get crop name
+		local mname = node.name:split(":")[1]
+		local pname = node.name:split(":")[2]
+		local sname = tonumber(pname:split("_")[2])
+		pname = pname:split("_")[1]
+
+		if not sname then
+			return
+		end
+
+		-- add dropped items
+		for _, dropped_item in pairs(drops) do
+
+			-- dont drop items on this list
+			for _, not_item in pairs({"farming:trellis", "farming:beanpole"}) do
+
+				if dropped_item == not_item then
+					dropped_item = nil
+				end
+			end
+
+			if dropped_item then
+
+				local obj = minetest.add_item(pos, dropped_item)
+
+				if obj then
+
+					obj:set_velocity({
+						x = math.random(-10, 10) / 9,
+						y = 3,
+						z = math.random(-10, 10) / 9,
+					})
+				end
+			end
+		end
+
+		-- Run script hook
+		for _, callback in pairs(core.registered_on_dignodes) do
+			callback(pos, node, placer)
+		end
+
+		-- play sound
+		minetest.sound_play("default_grass_footstep", {pos = pos, gain = 1.0})
+
+		local replace = mname .. ":" .. pname .. "_1"
+
+		if minetest.registered_nodes[replace] then
+
+			local p2 = minetest.registered_nodes[replace].place_param2 or 1
+
+			minetest.set_node(pos, {name = replace, param2 = p2})
+		else
+			minetest.set_node(pos, {name = "air"})
+		end
+
+		itemstack:add_wear(65535 / 500) -- 500 uses
+		return itemstack
+
+	end,
+})
+
+minetest.register_craft({
+	output = "fnuclear:uranium_sickle",
+	recipe = {
+	{"technic:uranium0_ingot", "technic:uranium0_ingot", ""},
+	{"", "", "technic:uranium0_ingot"},
+	{"", "default:stick", ""}
+  }
+})
+
+minetest.register_tool("fnuclear:uranium_hammer", {
+	description = "Uranium Hammer",
+	inventory_image = "uranium_hammer.png",
+	paramtype = "light",
+	light_source = 12,
+	on_use = function(itemstack, user, pointed_thing)
+	if pointed_thing.type ~= "node" then
+		return
+	end
+
+	local pos = pointed_thing.under
+
+	if minetest.is_protected(pos, user:get_player_name()) then
+		minetest.record_protection_violation(pos, user:get_player_name())
+		return
+	end
+
+	local node = minetest.get_node(pos)
+	if node.name == "default:cobble" or node.name == "default:mossycobble"
+		or node.name == "default:desert_cobble" or node.name == "default:stone"
+		or node.name == "default:desert_sandstone" or node.name == "default:desert_stone"
+		or node.name == "default:sandstone" or node.name == "default:silver_sandstone" then
+		node.name = "default:gravel"
+		minetest.swap_node(pos, node)
+		minetest.sound_play({
+			name="default_dig_crumbly"},{
+			gain=1,
+			pos=pos,
+			max_hear_distance=6,
+			loop=false})
+	elseif node.name == "default:gravel" or node.name == "gravelsieve:compressed_gravel" then
+		node.name = "default:sand"
+		minetest.swap_node(pos, node)
+		minetest.sound_play({
+			name="default_dig_crumbly"},{
+			gain=1,
+			pos=pos,
+			max_hear_distance=6,
+			loop=false})
+	end
+
+	itemstack:add_wear(65535 / 500)
+	return itemstack
+	end,
+})
+
+minetest.register_craft({
+	output = "fnuclear:uranium_hammer",
+	recipe = {
+		{"", "technic:uranium0_ingot", ""},
+		{"", "default:stick", "technic:uranium0_ingot"},
+		{"default:stick", "", ""},
+	}
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:mese_crystal_fragment 15",
+	recipe = {'default:pick_mese', 'fnuclear:uranium_hammer'},
+	replacements = {{'fnuclear:uranium_hammer', 'fnuclear:uranium_hammer'}},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:mese_crystal_fragment 15",
+	recipe = {'default:axe_mese', 'fnuclear:uranium_hammer'},
+	replacements = {{'fnuclear:uranium_hammer', 'fnuclear:uranium_hammer'}},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:mese_crystal_fragment 15",
+	recipe = {'default:shovel_mese', 'fnuclear:uranium_hammer'},
+	replacements = {{'fnuclear:uranium_hammer', 'fnuclear:uranium_hammer'}},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:mese_crystal_fragment 15",
+	recipe = {'default:sword_mese', 'fnuclear:uranium_hammer'},
+	replacements = {{'fnuclear:uranium_hammer', 'fnuclear:uranium_hammer'}},
+})
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:diamond",
+	recipe = {'default:pick_diamond', 'fnuclear:uranium_hammer'},
+	replacements = {{'fnuclear:uranium_hammer', 'fnuclear:uranium_hammer'}},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:diamond",
+	recipe = {'default:axe_diamond', 'fnuclear:uranium_hammer'},
+	replacements = {{'fnuclear:uranium_hammer', 'fnuclear:uranium_hammer'}},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:diamond",
+	recipe = {'default:shovel_diamond', 'fnuclear:uranium_hammer'},
+	replacements = {{'fnuclear:uranium_hammer', 'fnuclear:uranium_hammer'}},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:diamond",
+	recipe = {'default:sword_diamond', 'fnuclear:uranium_hammer'},
+	replacements = {{'fnuclear:uranium_hammer', 'fnuclear:uranium_hammer'}},
 })
